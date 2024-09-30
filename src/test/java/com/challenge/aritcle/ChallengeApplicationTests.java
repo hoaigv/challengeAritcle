12ceae88320719969263f50d8d787a9c7031e123
hoaigv <hoangminhhoai.gv2002@gmail.com>
2024-09-30 11:38:26 +0700

    init remote repository

---
+++ src/test/java/com/challenge/aritcle/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.aritcle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
